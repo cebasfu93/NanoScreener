fixed files for them to work as in the repo. No numerical bugs

Library/Plot.py
@@ -15,8 +15,20 @@ def plot_all_angs(cath):
 
     The angle of each analyte is plotted in transparency and the mean profile in opaque
     """
-    fig, axs = plt.subplots(figsize=(28, 28), ncols=len(AA_ordered), nrows=len(AA_ordered), sharex=True, sharey=True, subplot_kw={
-                            'xlim': (0, 5), 'ylim': (0, 180), 'xticks': [0, 2.5], 'yticks': [0, 60, 120]}, gridspec_kw={'hspace': 0.0, 'wspace': 0.0})
+    fig, axs = plt.subplots(
+        figsize=(28, 28),
+        ncols=len(AA_ordered),
+        nrows=len(AA_ordered),
+        sharex=True,
+        sharey=True,
+        subplot_kw={
+            "xlim": (0, 5),
+            "ylim": (0, 180),
+            "xticks": [0, 2.5],
+            "yticks": [0, 60, 120],
+        },
+        gridspec_kw={"hspace": 0.0, "wspace": 0.0},
+    )
     for a1, aa1 in enumerate(AA_ordered):
         axs[a1, 0].set_ylabel("{}\nAngle\ndeg".format(aa1), fontsize=Z - 4)
         axs[0, a1].set_title(aa1, fontsize=Z)
@@ -32,7 +44,7 @@ def plot_all_angs(cath):
                 axs[a1, a2].plot(x, mean_ang, lw=1, c=(0.9, 0.5, 0), alpha=1)
     for ax in axs.flatten():
         ax.tick_params(labelsize=Z)
-    plt.savefig("Plots/{}/all_angles.png".format(cath.rname), format='png')
+    plt.savefig("{}/Plots/{}/all_angles.png".format(cath.setup, cath.rname), format="png")
     plt.show()
     plt.close()
 
@@ -47,11 +59,24 @@ def plot_all_forces(cath):
 
     The force of each analyte is plotted in transparency and the mean profile in opaque
     """
-    fig, axs = plt.subplots(figsize=(28, 28), ncols=len(AA_ordered), nrows=len(AA_ordered), sharex=True, sharey=True, subplot_kw={
-                            'xlim': (0, 5), 'ylim': (-75, 75), 'xticks': [0, 2.5], 'yticks': [-75, -25, 25]},
-                            gridspec_kw={'hspace': 0.0, 'wspace': 0.0})
+    fig, axs = plt.subplots(
+        figsize=(28, 28),
+        ncols=len(AA_ordered),
+        nrows=len(AA_ordered),
+        sharex=True,
+        sharey=True,
+        subplot_kw={
+            "xlim": (0, 5),
+            "ylim": (-75, 75),
+            "xticks": [0, 2.5],
+            "yticks": [-75, -25, 25],
+        },
+        gridspec_kw={"hspace": 0.0, "wspace": 0.0},
+    )
     for a1, aa1 in enumerate(AA_ordered):
-        axs[a1, 0].set_ylabel("{}\nForce\nkcal\n".format(aa1) + r"$mol^{-1}nm^{-1}$", fontsize=Z - 4)
+        axs[a1, 0].set_ylabel(
+            "{}\nForce\nkcal\n".format(aa1) + r"$mol^{-1}nm^{-1}$", fontsize=Z - 4
+        )
         axs[0, a1].set_title(aa1, fontsize=Z)
         axs[-1, a1].set_xlabel("Distance\n(nm)", fontsize=Z - 4)
         for a2, aa2 in enumerate(AA_ordered):
@@ -65,7 +90,7 @@ def plot_all_forces(cath):
                 axs[a1, a2].plot(x, mean_f, lw=1, c=(0.5, 0, 0.4), alpha=1)
     for ax in axs.flatten():
         ax.tick_params(labelsize=Z)
-    plt.savefig("Plots/{}/all_forces.png".format(cath.rname), format='png')
+    plt.savefig("{}/Plots/{}/all_forces.png".format(cath.setup, cath.rname), format="png")
     plt.show()
     plt.close()
 
@@ -80,11 +105,24 @@ def plot_all_dgs(cath):
 
     The PMF profile of each analyte is plotted in transparency and the mean profile in opaque
     """
-    fig, axs = plt.subplots(figsize=(28, 28), ncols=len(AA_ordered), nrows=len(AA_ordered), sharex=True, sharey=True, subplot_kw={
-                            'xlim': (0, 5), 'ylim': (-30, 15), 'xticks': [0, 2.5], 'yticks': [-30, -15, 0]},
-                            gridspec_kw={'hspace': 0.0, 'wspace': 0.0})
+    fig, axs = plt.subplots(
+        figsize=(28, 28),
+        ncols=len(AA_ordered),
+        nrows=len(AA_ordered),
+        sharex=True,
+        sharey=True,
+        subplot_kw={
+            "xlim": (0, 5),
+            "ylim": (-30, 15),
+            "xticks": [0, 2.5],
+            "yticks": [-30, -15, 0],
+        },
+        gridspec_kw={"hspace": 0.0, "wspace": 0.0},
+    )
     for a1, aa1 in enumerate(AA_ordered):
-        axs[a1, 0].set_ylabel("{}\nFree energy\nkcal ".format(aa1) + r"$mol^{-1}$", fontsize=Z - 4)
+        axs[a1, 0].set_ylabel(
+            "{}\nFree energy\nkcal ".format(aa1) + r"$mol^{-1}$", fontsize=Z - 4
+        )
         axs[0, a1].set_title(aa1, fontsize=Z)
         axs[-1, a1].set_xlabel("Distance\n(nm)", fontsize=Z - 4)
         for a2, aa2 in enumerate(AA_ordered):
@@ -92,14 +130,14 @@ def plot_all_dgs(cath):
             x = cath.x.get(key, [])
             g = cath.g.get(key, [])
             if x != []:
-                axs[a1, a2].axhline(0, c='k', lw=1)
+                axs[a1, a2].axhline(0, c="k", lw=1)
                 for gibbs in g.T:
                     axs[a1, a2].plot(x, gibbs, lw=2, c=(0, 0.6, 1), alpha=0.1)
                 mean_g = np.mean(g, axis=1)
                 axs[a1, a2].plot(x, mean_g, lw=2.5, c=(0, 0.6, 1), alpha=1)
     for ax in axs.flatten():
         ax.tick_params(labelsize=Z)
-    plt.savefig("Plots/{}/all_gibbs.png".format(cath.rname), format='png')
+    plt.savefig("{}/Plots/{}/all_gibbs.png".format(cath.setup, cath.rname), format="png")
     plt.show()
     plt.close()
 
@@ -115,9 +153,20 @@ def plot_dg_distributions(cath):
     The PMF profile of each analyte is plotted in transparency and the mean profile in opaque
     """
     g_space = np.linspace(-30, 15, 500)
-    fig, axs = plt.subplots(figsize=(28, 28), ncols=len(AA_ordered), nrows=len(AA_ordered), sharex=True, sharey=True, subplot_kw={
-                            'xlim': (-30, 15), 'xticks': [-30, -15, 0], 'ylim': (0, 3.0), 'yticks': [0, 0.5, 1.0, 1.5, 2.0, 2.5]},
-                            gridspec_kw={'hspace': 0.0, 'wspace': 0.0})
+    fig, axs = plt.subplots(
+        figsize=(28, 28),
+        ncols=len(AA_ordered),
+        nrows=len(AA_ordered),
+        sharex=True,
+        sharey=True,
+        subplot_kw={
+            "xlim": (-30, 15),
+            "xticks": [-30, -15, 0],
+            "ylim": (0, 3.0),
+            "yticks": [0, 0.5, 1.0, 1.5, 2.0, 2.5],
+        },
+        gridspec_kw={"hspace": 0.0, "wspace": 0.0},
+    )
     for a1, aa1 in enumerate(AA_ordered):
         axs[a1, 0].set_ylabel("{}\nProb.\ndensity".format(aa1), fontsize=Z)
         axs[0, a1].set_title(aa1, fontsize=Z)
@@ -133,12 +182,14 @@ def plot_dg_distributions(cath):
                 # axs[a1,a2].axvline(g_mean, ymin=0, ymax=np.max(gauss)/0.15, color=(0,0.3,0.5), lw=2, ls='-')
     for ax in axs.flatten():
         ax.tick_params(labelsize=Z)
-    plt.savefig("Plots/{}/all_dg_distr.png".format(cath.rname), format='png')
+    plt.savefig("{}/Plots/{}/all_dg_distr.png".format(cath.setup, cath.rname), format="png")
     plt.show()
     plt.close()
 
 
-def plot_dg_map(cath, show_mean=True, show_std=False, svg=False, THRESH=0, dg_min=3, dg_max=18):
+def plot_dg_map(
+    cath, show_mean=True, show_std=False, svg=False, THRESH=0, dg_min=3, dg_max=18
+):
     """
     cath is a Catecholamine_PMF object (see Preprocessing.py)
 
@@ -149,7 +200,7 @@ def plot_dg_map(cath, show_mean=True, show_std=False, svg=False, THRESH=0, dg_mi
     If svg, the plot is saved in .svg format
     dg_min and dg_max set the lower and upper limits of the colormap
     """
-    cmap = 'plasma'
+    cmap = "plasma"
     pic = np.zeros((len(AA_ordered), len(AA_ordered)))
     pic_downlim = np.zeros_like(pic)
     for a1, aa1 in enumerate(AA_ordered):
@@ -157,37 +208,54 @@ def plot_dg_map(cath, show_mean=True, show_std=False, svg=False, THRESH=0, dg_mi
             key = "{}-{}".format(aa1, aa2).upper()
             pic[a1, a2] = -1 * cath.g_mean.get(key, np.nan)
             # if the mean - std is lower than THRESH, then it should also be visible. This array does that
-            pic_downlim[a1, a2] = -1 * (cath.g_mean.get(key, np.nan) - cath.g_std.get(key, np.nan))
+            pic_downlim[a1, a2] = -1 * (
+                cath.g_mean.get(key, np.nan) - cath.g_std.get(key, np.nan)
+            )
     fig, ax = plt.subplots(figsize=(9, 9), ncols=1, nrows=1)
     cax = ax.imshow(pic, cmap=cmap, vmin=dg_min, vmax=dg_max)
     # This is just an overlapping plot that will make more opaque the cells corresponding to weak binders
     pic2 = pic_downlim.copy()
     pic2[pic2 < THRESH - 1.0] = 0  # 1.0 is expected deviation from PMF methods
     pic2[pic2 >= THRESH - 1.0] = 20  # 1.0 is expected deviation from PMF methods
-    ax.imshow(pic2, cmap='binary_r', alpha=0.3)
+    ax.imshow(pic2, cmap="binary_r", alpha=0.3)
     ax.xaxis.tick_top()
-    ax.xaxis.set_label_position('top')
+    ax.xaxis.set_label_position("top")
     ax.set_xticks(np.linspace(0, len(AA_ordered) - 1, len(AA_ordered)))
-    ax.set_xticks(np.linspace(-0.5, len(AA_ordered) - 0.5, len(AA_ordered) + 1), minor=True)
+    ax.set_xticks(
+        np.linspace(-0.5, len(AA_ordered) - 0.5, len(AA_ordered) + 1), minor=True
+    )
     ax.set_xticklabels(AA_ordered)
     ax.set_yticks(np.linspace(0, len(AA_ordered) - 1, len(AA_ordered)))
-    ax.set_yticks(np.linspace(-0.5, len(AA_ordered) - 0.5, len(AA_ordered) + 1), minor=True)
+    ax.set_yticks(
+        np.linspace(-0.5, len(AA_ordered) - 0.5, len(AA_ordered) + 1), minor=True
+    )
     ax.set_yticklabels(AA_ordered)
     ax.tick_params(labelsize=Z, size=0, width=1.6)
-    ax.set_ylabel('AA1', fontsize=Z + 4)
-    ax.set_xlabel('AA2', fontsize=Z + 4)
-    ax.xaxis.grid(which='minor', lw=2, c='k')
-    ax.yaxis.grid(which='minor', lw=2, c='k')
+    ax.set_ylabel("AA1", fontsize=Z + 4)
+    ax.set_xlabel("AA2", fontsize=Z + 4)
+    ax.xaxis.grid(which="minor", lw=2, c="k")
+    ax.yaxis.grid(which="minor", lw=2, c="k")
 
     if show_mean:
         for a1, aa1 in enumerate(AA_ordered):
             for a2, aa2 in enumerate(AA_ordered):
-                if pic[a1, a2] > THRESH - 1.0:  # THRESHOLD. 1.0 is expected deviation from PMF methods
+                if (
+                    pic[a1, a2] > THRESH - 1.0
+                ):  # THRESHOLD. 1.0 is expected deviation from PMF methods
                     label = "{:.1f}".format(pic[a1, a2])
                     if show_std:
-                        label += "\n{:.1f}".format(cath.g_std["{}-{}".format(aa1, aa2).upper()])
-                    ax.text(a2, a1, label, color='k', ha='center',
-                            va='center', fontsize=Z - 1)  # , weight='bold'
+                        label += "\n{:.1f}".format(
+                            cath.g_std["{}-{}".format(aa1, aa2).upper()]
+                        )
+                    ax.text(
+                        a2,
+                        a1,
+                        label,
+                        color="k",
+                        ha="center",
+                        va="center",
+                        fontsize=Z - 1,
+                    )  # , weight='bold'
 
     a = plt.axes([0.95, 0.125, 0.04, 0.76])
     cbar = fig.colorbar(cax, ax=a, cax=a, ticks=[3, 6, 9, 12, 13.6, 15, 18])
@@ -196,12 +264,24 @@ def plot_dg_map(cath, show_mean=True, show_std=False, svg=False, THRESH=0, dg_mi
     cbar.ax.set_ylim(dg_min, dg_max)
 
     if svg:
-        plt.savefig("Plots/{}/all_dg_map.png".format(cath.rname),
-                    format='png', bbox_inches='tight', dpi=300)
-        plt.savefig("Plots/{}/all_dg_map.svg".format(cath.rname), format='svg', bbox_inches='tight')
+        plt.savefig(
+            "{}/Plots/{}/all_dg_map.png".format(cath.setup, cath.rname),
+            format="png",
+            bbox_inches="tight",
+            dpi=300,
+        )
+        plt.savefig(
+            "{}/Plots/{}/all_dg_map.svg".format(cath.setup, cath.rname),
+            format="svg",
+            bbox_inches="tight",
+        )
     else:
-        plt.savefig("Plots/{}/all_dg_map.png".format(cath.rname),
-                    format='png', bbox_inches='tight', dpi=300)
+        plt.savefig(
+            "{}/Plots/{}/all_dg_map.png".format(cath.setup, cath.rname),
+            format="png",
+            bbox_inches="tight",
+            dpi=300,
+        )
     plt.show()
     plt.close()
 
@@ -219,28 +299,43 @@ def plot_aa_dependence(cath):
     colors = cm.terrain(np.linspace(0, 0.85, len(AA_ordered)))
     faded_colors = [(*c[:3], 0.8) for c in colors]
     faded_colors = [(*c[:3], 0.4) for c in colors]
-    bp = dict(linestyle='-', lw=1.5, color='k', facecolor='r')
-    fp = dict(marker='o', ms=8, ls='none', mec='k', mew=2)
-    mp = dict(ls='-', lw=1., color='k')
-    cp = dict(ls='-', lw=1.5, color='k')
-    wp = dict(ls='-', lw=1.5, color='k')
+    bp = dict(linestyle="-", lw=1.5, color="k", facecolor="r")
+    fp = dict(marker="o", ms=8, ls="none", mec="k", mew=2)
+    mp = dict(ls="-", lw=1.0, color="k")
+    cp = dict(ls="-", lw=1.5, color="k")
+    wp = dict(ls="-", lw=1.5, color="k")
 
-    fig, axs = plt.subplots(figsize=(7, 5), ncols=1, nrows=2, gridspec_kw={'hspace': 0.4})
+    fig, axs = plt.subplots(
+        figsize=(7, 5), ncols=1, nrows=2, gridspec_kw={"hspace": 0.4}
+    )
     plt.ylabel("Free energy (kcal" + r"$mol^{-1}$)", fontsize=Z)
-    grouping_col = ['aa1', 'aa2']
-    xlabels = ['AA1', 'AA2']
+    grouping_col = ["aa1", "aa2"]
+    xlabels = ["AA1", "AA2"]
 
     xx = np.linspace(1, len(AA_ordered), len(AA_ordered))
     for ax, g_col, xlab in zip(axs, grouping_col, xlabels):
         ax.tick_params(labelsize=Z, size=6, width=1.6)
         tmp_df = cath.data.copy()
         tmp_df.dgs = tmp_df.dgs.apply(list)
-        g_pts = tmp_df.groupby(g_col).agg({'dgs': 'sum'}).dgs.loc[[aa.upper() for aa in AA_ordered]]
+        g_pts = (
+            tmp_df.groupby(g_col)
+            .agg({"dgs": "sum"})
+            .dgs.loc[[aa.upper() for aa in AA_ordered]]
+        )
         for g_aa, i, c, fc in zip(g_pts, xx, colors, faded_colors):
-            bpl = ax.boxplot(g_aa, positions=[i], widths=0.6, patch_artist=True,
-                             boxprops=bp, flierprops=fp, medianprops=mp, capprops=cp, whiskerprops=wp)
-            bpl['boxes'][0].set_facecolor(c)
-            bpl['fliers'][0].set_markerfacecolor(fc)
+            bpl = ax.boxplot(
+                g_aa,
+                positions=[i],
+                widths=0.6,
+                patch_artist=True,
+                boxprops=bp,
+                flierprops=fp,
+                medianprops=mp,
+                capprops=cp,
+                whiskerprops=wp,
+            )
+            bpl["boxes"][0].set_facecolor(c)
+            bpl["fliers"][0].set_markerfacecolor(fc)
         ax.set_ylim(-30, 0)
         ax.set_xlim(0.5, len(AA_ordered) + 0.5)
         ax.set_xticks(xx)
@@ -249,8 +344,16 @@ def plot_aa_dependence(cath):
         ax.set_xlabel(xlab, fontsize=Z)
         # ax.set_ylabel("Free energy\n(kcal"+r"$mol^{-1}$)", fontsize=Z)
         ax.grid()
-    plt.savefig("Plots/{}/aa_dependence.svg".format(cath.rname), format='svg', bbox_inches='tight')
-    plt.savefig("Plots/{}/aa_dependence.png".format(cath.rname),
-                format='png', dpi=300, bbox_inches='tight')
+    plt.savefig(
+        "{}/Plots/{}/aa_dependence.svg".format(cath.setup, cath.rname),
+        format="svg",
+        bbox_inches="tight",
+    )
+    plt.savefig(
+        "{}/Plots/{}/aa_dependence.png".format(cath.setup, cath.rname),
+        format="png",
+        dpi=300,
+        bbox_inches="tight",
+    )
     plt.show()
     plt.close()

Library/Preprocessing.py
@@ -12,11 +12,11 @@ def rank_metric(colname, data):
 
     Return a dataframe with all the columns (old and new)
     """
-    mean_col = colname + '_mean'
-    std_col = colname + '_std'
-    rank_col = colname + '_rank'
-    rank_std_up_col = colname + '_rank_std_up'
-    rank_std_down_col = colname + '_rank_std_down'
+    mean_col = colname + "_mean"
+    std_col = colname + "_std"
+    rank_col = colname + "_rank"
+    rank_std_up_col = colname + "_rank_std_up"
+    rank_std_down_col = colname + "_rank_std_down"
     Rank_Std_up = []
     Rank_Std_down = []
 
@@ -25,15 +25,22 @@ def rank_metric(colname, data):
 
     for i in range(len(data)):
         # -1 to not count itself
-        lower_than = np.sum((data[mean_col].iloc[i] + data[std_col].iloc[i]) >=
-                            (data[mean_col].iloc[i:] - data[std_col].iloc[i:])) - 1
+        lower_than = (
+            np.sum(
+                (data[mean_col].iloc[i] + data[std_col].iloc[i])
+                >= (data[mean_col].iloc[i:] - data[std_col].iloc[i:])
+            )
+            - 1
+        )
         Rank_Std_up.append(lower_than)
         # -1 is absent because of slicing open/closed limits
-        greater_than = np.sum((data[mean_col].iloc[i] - data[std_col].iloc[i])
-                              <= (data[mean_col].iloc[:i] + data[std_col].iloc[:i]))
+        greater_than = np.sum(
+            (data[mean_col].iloc[i] - data[std_col].iloc[i])
+            <= (data[mean_col].iloc[:i] + data[std_col].iloc[:i])
+        )
         Rank_Std_down.append(greater_than)
-        data['menos'] = data[mean_col] - data[std_col]
-        data['mas'] = data[mean_col] + data[std_col]
+        data["menos"] = data[mean_col] - data[std_col]
+        data["mas"] = data[mean_col] + data[std_col]
     data[rank_std_up_col] = Rank_Std_up
     data[rank_std_down_col] = Rank_Std_down
     data.sort_index(inplace=True)
@@ -61,7 +68,7 @@ def gaussian(x, mu, sig):
     Returns a gaussian centered in mu and with variance sig
     x can be a 1D array or a single numeric value
     """
-    return np.exp(-0.5 * np.power((x - mu) / sig, 2.)) / (sig * (2 * np.pi)**0.5)
+    return np.exp(-0.5 * np.power((x - mu) / sig, 2.0)) / (sig * (2 * np.pi) ** 0.5)
 
 
 def boltz_mean(x, T=300):
@@ -85,7 +92,7 @@ def boltz_std(x, T=300):
     mean2 = np.sum(np.multiply(np.exp(x / RT), x**2)) / np.sum(np.exp(x / RT))
     mean = np.sum(np.multiply(np.exp(x / RT), x)) / np.sum(np.exp(x / RT))
     # return (np.sum(np.multiply(np.exp(x/RT), (x-mean)**2))/np.sum(np.exp(x/RT)))**0.5 #two returns equivalent
-    return (mean2 - mean**2)**0.5
+    return (mean2 - mean**2) ** 0.5
     # return np.std(x)/(len(x)**0.5)
 
 
@@ -94,7 +101,9 @@ def reject_outliers(data, iq=1.5):
     Slices the 1D array data keeping only the values within iq times the interquartile distance from the first and third quartiles
     """
     IQ = np.quantile(data, 0.75) - np.quantile(data, 0.25)
-    mask = np.logical_and(data < np.median(data) + iq * IQ, data > np.median(data) - iq * IQ)
+    mask = np.logical_and(
+        data < np.median(data) + iq * IQ, data > np.median(data) - iq * IQ
+    )
     return np.array(data)[mask]
 
 
@@ -112,18 +121,20 @@ def read_file(fname):
     for line in fl:
         if "#" not in line and "@" not in line:
             clean.append(line.split())
-    clean = np.array(clean, dtype='float')
+    clean = np.array(clean, dtype="float")
     return clean
 
 
-def read_dang(rname, nice_x, key):
+def read_dang(rname, nice_x, key, setup):
     """
     Imports angles from text file located at "DANGS/{rname}/{key}-ASX_PULL_dang.sfu"
     The function returns an array with the angles at the distances in nice_x
     """
-    dang = read_file("DANGS/{}/{}-ASX_PULL_dang.sfu".format(rname, key))
-    d = dang[:, :len(dang[0, :]) // 2] * 10  # *10 because of a unit error in the dangs python script
-    ang = dang[:, (len(dang[0, :]) // 2):]
+    dang = read_file("{}/DANGS/{}/{}-ASX_PULL_dang.sfu".format(setup, rname, key))
+    d = (
+        dang[:, : len(dang[0, :]) // 2] * 10
+    )  # *10 because of a unit error in the dangs python script
+    ang = dang[:, (len(dang[0, :]) // 2) :]
     nice_ang = []
     for i in range(len(d[0, :])):
         angles = []
@@ -158,7 +169,7 @@ def read_text_file_pull(fname):
     Ignores lines with "#" and "@"
     Return array
     """
-    f = open(fname, 'r')
+    f = open(fname, "r")
     fl = f.readlines()
     f.close()
 
@@ -166,34 +177,38 @@ def read_text_file_pull(fname):
     for line in fl[:-1]:
         if "@" not in line and "#" not in line:
             data.append(line.split())
-    data = np.array(data, dtype='float')
+    data = np.array(data, dtype="float")
     return data
 
 
-def import_pullx(rname):
+def import_pullx(rname, setup):
     """
-    Reads text file with path "PULLX/{rname}/{aa1}-{aa2}-ASX_PULL_pullx.xvg",
+    Reads text file with path "{setup}/PULLX/{rname}/{aa1}-{aa2}-ASX_PULL_pullx.xvg",
     where aa1 and aa2 are values in the AA_pairs list (see Constants.py)
     Ignores lines with "#" and "@"
     Returns dictionary
     """
     pullxs = {}
     for a1, a2 in AA_pairs:
-        pullx = read_text_file_pull("PULLX/{}/{}-{}-ASX_PULL_pullx.xvg".format(rname, a1, a2))
+        pullx = read_text_file_pull(
+            "{}/PULLX/{}/{}-{}-ASX_PULL_pullx.xvg".format(setup, rname, a1, a2)
+        )
         pullxs["{}-{}".format(a1, a2)] = pullx
     return pullxs
 
 
-def import_pullf(rname):
+def import_pullf(rname, setup):
     """
-    Reads text file with path "PULLF/{rname}/{aa1}-{aa2}-ASX_PULL_pullf.xvg",
+    Reads text file with path "{setup}/PULLF/{rname}/{aa1}-{aa2}-ASX_PULL_pullf.xvg",
     where aa1 and aa2 are values in the AA_pairs list (see Constants.py)
     Ignores lines with "#" and "@"
     Returns dictionary
     """
     pullfs = {}
     for a1, a2 in AA_pairs:
-        pullf = read_text_file_pull("PULLF/{}/{}-{}-ASX_PULL_pullf.xvg".format(rname, a1, a2))
+        pullf = read_text_file_pull(
+            "{}/PULLF/{}/{}-{}-ASX_PULL_pullf.xvg".format(setup, rname, a1, a2)
+        )
         pullfs["{}-{}".format(a1, a2)] = pullf
     return pullfs
 
@@ -235,8 +250,12 @@ def homogenize_pull(X, F, n_ignore=40, n_smooth=1000):
         dg = np.cumsum(f * dx)
         nice_g.append(dg)
     nice_g = np.array(nice_g).T
-    nice_g = (nice_g[n_ignore:-n_ignore] - nice_g[n_ignore:-n_ignore][-1])
-    return nice_x[n_ignore:-n_ignore], nice_f[n_ignore:-n_ignore] / 4.184, nice_g / 4.184  # kJ to kcal"""
+    nice_g = nice_g[n_ignore:-n_ignore] - nice_g[n_ignore:-n_ignore][-1]
+    return (
+        nice_x[n_ignore:-n_ignore],
+        nice_f[n_ignore:-n_ignore] / 4.184,
+        nice_g / 4.184,
+    )  # kJ to kcal"""
 
 
 def protein_similitude_score(data):
@@ -246,8 +265,10 @@ def protein_similitude_score(data):
     """
     max_freq = max(PDB_frequencies.values())
     max_score = 2 * max_freq
-    score = [0.5 * (PDB_frequencies[aa1] + PDB_frequencies[aa2]) / max_freq
-             for aa1, aa2 in data[['aa1', 'aa2']].values]
+    score = [
+        0.5 * (PDB_frequencies[aa1] + PDB_frequencies[aa2]) / max_freq
+        for aa1, aa2 in data[["aa1", "aa2"]].values
+    ]
     return score
 
 
@@ -270,24 +291,43 @@ class Catecholamine_PMF:
     naturally ocurring binding pockets of catecholamines
     """
 
-    def __init__(self, rname):
+    def __init__(self, rname, setup):
         self.rname = rname
-        pullx = import_pullx(rname)
-        pullf = import_pullf(rname)
+        self.setup = setup
+        pullx = import_pullx(rname, setup=self.setup)
+        pullf = import_pullf(rname, setup=self.setup)
         self.keys = ["{}-{}".format(a1, a2) for a1, a2 in AA_pairs]
-        self.x, self.f, self.g, self.g_bind, self.g_mean, self.g_std, self.dangs = {}, {}, {}, {}, {}, {}, {}
+        self.x, self.f, self.g, self.g_bind, self.g_mean, self.g_std, self.dangs = (
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+        )
         for key in self.keys:
             print(key)
-            self.x[key], self.f[key], self.g[key] = homogenize_pull(pullx[key], pullf[key])
-            self.g_bind[key] = reject_outliers([np.min(gibbs) for gibbs in self.g[key].T], iq=1.5)
+            self.x[key], self.f[key], self.g[key] = homogenize_pull(
+                pullx[key], pullf[key]
+            )
+            self.g_bind[key] = reject_outliers(
+                [np.min(gibbs) for gibbs in self.g[key].T], iq=1.5
+            )
             self.g_mean[key] = boltz_mean(self.g_bind[key])
             self.g_std[key] = boltz_std(self.g_bind[key])
-            self.dangs[key] = read_dang(rname, self.x[key], key)
+            self.dangs[key] = read_dang(rname, self.x[key], key, setup=self.setup)
 
-        colnames = ['aa1', 'aa2', 'dgs', 'dg_mean', 'dg_std']
-        core = [[key[:3], key[4:7], g, g_av, g_dev]
-                for key, g, g_av, g_dev
-                in zip(self.keys, self.g_bind.values(), self.g_mean.values(), self.g_std.values())]
+        colnames = ["aa1", "aa2", "dgs", "dg_mean", "dg_std"]
+        core = [
+            [key[:3], key[4:7], g, g_av, g_dev]
+            for key, g, g_av, g_dev in zip(
+                self.keys,
+                self.g_bind.values(),
+                self.g_mean.values(),
+                self.g_std.values(),
+            )
+        ]
         self.data = pd.DataFrame(core, columns=colnames)
         self.protein_similitude = protein_similitude_score(self.data)
-        self.data['psc'] = self.protein_similitude
+        self.data["psc"] = self.protein_similitude